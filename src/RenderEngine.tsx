f32a0bc31d1159ccc88ee3faf7a52e8bf4beabad
fortune_cookie <767077147@qq.com>
2022-11-14 18:04:24 +0800

    fix: 修改类型引入路径

--- src/RenderEngine.tsx
+++ src/RenderEngine.tsx
@@ -1,33 +1,34 @@
-import { useUpdate } from 'ahooks';
-import React, { FC, useEffect } from 'react';
+import { useUpdate } from 'ahooks'
+import React, { FC, useEffect } from 'react'
 
-import DroppableArea from '@/components/DroppableArea';
-import { EngineContext } from '@/context';
-import { useEngine } from '@/hooks/useEngine';
-import useEventEmitter from '@/hooks/useEventEmitter';
-import { RenderEngineProps } from '@/types';
+import { RenderEngineProps } from './types'
 
-import './styles/base.less';
+import DroppableArea from '@/components/DroppableArea'
+import { EngineContext } from '@/context'
+import { useEngine } from '@/hooks/useEngine'
+import useEventEmitter from '@/hooks/useEventEmitter'
+
+import './styles/base.less'
 
 const RenderEngine: FC<RenderEngineProps> = ({ dataSource }) => {
-  const globalEngine = useEngine();
-  const eventEmitter = useEventEmitter();
-  const update = useUpdate();
+  const globalEngine = useEngine()
+  const eventEmitter = useEventEmitter()
+  const update = useUpdate()
 
   useEffect(() => {
     if (dataSource) {
-      const { cmpList = [], pageConfig = {} } = dataSource;
-      globalEngine.updateCmps(cmpList);
-      globalEngine.updatePageConfig(pageConfig);
-      update();
+      const { cmpList = [], pageConfig = {} } = dataSource
+      globalEngine.updateCmps(cmpList)
+      globalEngine.updatePageConfig(pageConfig)
+      update()
     }
-  }, [dataSource]);
+  }, [dataSource])
 
   return (
     <EngineContext.Provider value={{ globalEngine, eventEmitter }}>
       <DroppableArea />
     </EngineContext.Provider>
-  );
-};
+  )
+}
 
-export default RenderEngine;
+export default RenderEngine
